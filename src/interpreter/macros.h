a122b6c12ab80ab736faabbf1fa54fd048e3e561
Yuan Cao <caoyuan0816@gmail.com>
2018-04-12 09:31:21 -0700

    Restructure /interpreter, prepare for test

--- src/interpreter/macros.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUNA_MACRO_H
-#define LUNA_MACRO_H
-
-#define LOG(x) std::cout << x << std::endl;
-#define LOGI(x) std::cout << x << " ";
-#define LOGE(x) std::cerr << x << std::endl;
-
-#endif

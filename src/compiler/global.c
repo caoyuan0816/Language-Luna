cb7c7e455dbb105715ffe6435ff1e68672d82bc4
CSE360-Team5-Lin <zbao7@asu.edu>
2018-04-18 16:09:16 -0700

    c function

--- src/compiler/global.c
+++ src/compiler/global.c
@@ -1,3 +1,13 @@
 #include "global.h"
 
 int errorOccur = 0;
+
+ast_node newast(int line. char* name, AST_TREE n1, AST_TREE n2){
+    ast_node t ;
+    t = (ast_node)malloc(sizeof(AST_NODE));
+    t->line = line;
+    strcpy(t->name, name);
+    t->left = n1;
+    t-> rigth = n2;
+    return (t);
+}

0574e41410a1ad65e23f9a3446492a5da3f1ee87
CSE360-Team5-Lin <zbao7@asu.edu>
2018-04-17 22:32:13 -0700

    add nodes

--- src/compiler/global.h
+++ src/compiler/global.h
@@ -1,7 +1,42 @@
 #ifndef GLOBAL_H_
 #define GLOBAL_H_
 
+int errorOccur = 0;
+extern int yylineno;//行号
+extern char* yytext;//词
+
 /*tree node definition*/
+
+struct ast_node {
+    int line;
+    char* name;
+    char *instructor;
+    char* type;
+    struct ast_node *left;
+    struct ast_node *right;
+};
+
+
+//for function
+struct ast_function_node // for function calls
+{
+    int node_type;
+    char* name;
+    char* type;
+    struct ast_node * arguments;
+    
+    struct symbol_node * symbol;
+};
+
+struct number_node{
+    int line;
+    char* name;
+}
+
+
+
+
+
 
 
 #endif // GLOBAL_H_
